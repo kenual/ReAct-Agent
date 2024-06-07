0954d61e6bfe2992bdf416883cb2a0c7e9fdad2e
kenual <kenneth_lau@yahoo.com>
2024-06-07 04:48:02 -0700

    Add query loop

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from llama_index.core import set_global_handler
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.ollama import Ollama
-from llama_index.tools.wikipedia import WikipediaToolSpec
+from llama_index.tools.duckduckgo import DuckDuckGoSearchToolSpec
+
+set_global_handler("simple")
 
 llm = Ollama(model="phi3", request_timeout=60)
-agent = ReActAgent.from_tools(WikipediaToolSpec().to_tool_list(), llm=llm, verbose=True)
+agent = ReActAgent.from_tools(
+    DuckDuckGoSearchToolSpec().to_tool_list(), llm=llm, verbose=True)
 
-response = agent.chat("What is LlamaIndex?")
-print(response)
+if __name__ == "__main__":
+    while True:
+        user_input = input('Enter query> ')
+        if user_input.lower() in ['', 'exit', 'quit']:
+            break
+        
+        response = agent.chat("user_input")
+        print(response)
